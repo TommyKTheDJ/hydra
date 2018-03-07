286e462a0b1e9da21246ceae52ea2fef457a6108
Nicola Arnoldi <9900073+dustnic@users.noreply.github.com>
2018-03-07 18:27:44 +0000

    cumulus initial config

--- install_aos_agents.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --rm \
--v /Users/arnoldin/Projects/hydra/inventory/physical_inventory:/aos/physical_inventory \
--v /Users/arnoldin/Projects/hydra/playbooks/group_vars/cumulus/:/aos/group_vars/cumulus/ \
-aos-device-installer \
-install \
---inventory physical_inventory \
---limit cumulus
