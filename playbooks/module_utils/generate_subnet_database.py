f2056e0afcca500b8e848175d5a00391485d558e
Nicola Arnoldi <9900073+dustnic@users.noreply.github.com>
2018-04-29 16:25:18 +0100

    various fixes (initial python install, subnet generation and workers initial config)

--- playbooks/module_utils/generate_subnet_database.py
+++ playbooks/module_utils/generate_subnet_database.py
@@ -1,17 +1,14 @@
 from ansible.module_utils.common_functions import *
 
 def generate_subnet_database(inventory_filename = None):
-    interfaces =  xls_to_dict(inventory_filename=inventory_filename, sheet_name="interfaces")
+    interfaces =  xls_to_dict(inventory_filename=inventory_filename, sheet_name="interfaces",index_col=[0,1])
     subnets =  xls_to_dict(inventory_filename=inventory_filename, sheet_name="subnets")
-
-
     data = {}
     data['subnets'] = {}
-    for name, values in subnets.iteritems():
-
+    for subnet_name, values in subnets.iteritems():
         subnet = {}
         if not is_empty_datum(values['vlan_id']): subnet['vlan_id'] = values['vlan_id']
-        subnet['name'] = name
+        subnet['name'] = subnet_name
         subnet['notes'] = values['notes']
         subnet['network'] = values['network']
         subnet['prefix_length'] = values['prefix_length']
@@ -24,12 +21,14 @@ def generate_subnet_database(inventory_filename = None):
             if not is_empty_datum(values['zone'])  :
                 subnet['domain'] = ".".join([values['zone'],values['domain']])
         subnet['hosts'] = []
-        for hostname,interface_data in interfaces.iteritems():
-            if interface_data['subnet'] == name:
+
+
+        for (hostname,interface_subnet_name),interface_data in interfaces.iteritems():
+            if interface_subnet_name == subnet_name:
                 host = {}
                 host['hostname'] = hostname
                 if not is_empty_datum(interface_data['mac_address']): host['mac_address'] = format_mac_address(interface_data['mac_address'])
                 subnet['hosts'].append(host)
 
-        data['subnets'][name] = subnet
+        data['subnets'][subnet_name] = subnet
     return data
